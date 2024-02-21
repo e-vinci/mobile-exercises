6c84482b4cd49bc2de22cf961fcbc1f55cc0e682
Sébastien Strebelle <sebastien.strebelle@vinci.be>
2024-02-21 15:31:02 +0100

    T01.4 ajout d’une image

--- tutoriels/tuto1/lib/main.dart
+++ tutoriels/tuto1/lib/main.dart
@@ -25,42 +25,49 @@ class MyApp extends StatelessWidget {
             padding: const EdgeInsets.all(8.0),
             child: SizedBox(
               width: 500,
-              child: Column(
+              child: Row(
                 children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const Text(
-                        "iPhone 15 Pro Max",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16,
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const Text(
+                              "iPhone 15 Pro Max",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                              ),
+                            ),
+                            Text(
+                              "1479 €",
+                              style: TextStyle(
+                                color: Theme.of(context).colorScheme.primary,
+                                fontSize: 16,
+                              ),
+                            ),
+                          ],
                         ),
-                      ),
-                      Text(
-                        "1479 €",
-                        style: TextStyle(
-                          color: Theme.of(context).colorScheme.primary,
-                          fontSize: 16,
+                        const SizedBox(height: 8),
+                        const Text(
+                          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
+                              "Aliquam et risus vel ipsum faucibus ultrices. "
+                              "Fusce nec leo nisi. Vestibulum vehicula, "
+                              "orci ac varius eleifend, ante erat efficitur tortor, "
+                              "quis tincidunt elit ex ut est.",
+                          textAlign: TextAlign.justify,
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                  const SizedBox(height: 8),
-                  const Text(
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
-                        "Aliquam et risus vel ipsum faucibus ultrices. "
-                        "Fusce nec leo nisi. Vestibulum vehicula, "
-                        "orci ac varius eleifend, ante erat efficitur tortor, "
-                        "quis tincidunt elit ex ut est.",
-                    textAlign: TextAlign.justify,
-                  )
+                  const SizedBox(width: 16),
+                  Image.asset('images/iphone.jpg', width: 150),
                 ],
               ),
             ),
           ),
         ),
-
       ),
     );
   }
