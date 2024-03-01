422be0bc445c31a54d8e2b2e6ca831021ab4f49d
Sébastien Strebelle <sebastien.strebelle@vinci.be>
2024-03-01 09:47:39 +0100

    T02.5 Filtrage de liste

--- tutoriels/tuto2/lib/home_screen.dart
+++ tutoriels/tuto2/lib/home_screen.dart
@@ -3,25 +3,44 @@ import 'package:flutter/material.dart';
 import 'contact.dart';
 import 'contact_row.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  var showFavorites = false;
+
+  void _toggleFavorites() => setState(() => showFavorites = !showFavorites);
+
+  @override
   Widget build(BuildContext context) {
+    final displayedContacts = [
+      for (var contact in defaultContacts)
+        if (!showFavorites || contact.isFavorite) contact
+    ];
+
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text("Contact list"),
+        actions: [
+          IconButton(
+            icon: Icon(showFavorites ? Icons.star : Icons.star_border),
+            onPressed: _toggleFavorites,
+          ),
+        ],
       ),
-      body: Padding(
+      body: Container(
         padding: const EdgeInsets.all(16.0),
         child: Center(
           child: SizedBox(
-            width: 512,
+            width: 512.0,
             child: ListView.builder(
-              itemCount: defaultContacts.length,
+              itemCount: displayedContacts.length,
               itemBuilder: (context, index) =>
-                  ContactRow(contact: defaultContacts[index]),
+                  ContactRow(contact: displayedContacts[index]),
             ),
           ),
         ),
