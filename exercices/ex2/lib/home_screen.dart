7020175e1118ae737f4aed242027abf9a18c1669
Sébastien Strebelle <sebastien.strebelle@vinci.be>
2024-03-01 10:03:15 +0100

    F03.1 Sélection de mode

--- exercices/ex2/lib/home_screen.dart
+++ exercices/ex2/lib/home_screen.dart
@@ -3,12 +3,20 @@ import 'package:flutter/material.dart';
 import 'property.dart';
 import 'property_widget.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  bool isSales = true;
+
+  @override
   Widget build(BuildContext context) {
-    final properties = [...propertiesToSell, ...propertiesToRent];
+    final properties = isSales ? propertiesToSell : propertiesToRent;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Properties"),
@@ -16,11 +24,36 @@ class HomeScreen extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: ListView.separated(
-          itemCount: properties.length,
-          separatorBuilder: (context, index) => const Divider(),
-          itemBuilder: (context, index) =>
-              PropertyWidget(property: properties[index]),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "Rentals",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(width: 8),
+                Switch(
+                  value: isSales,
+                  onChanged: (value) => setState(() => isSales = !isSales),
+                ),
+                const SizedBox(width: 8),
+                const Text(
+                  "Sales",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                )
+              ],
+            ),
+            Expanded(
+              child: ListView.separated(
+                itemCount: properties.length,
+                separatorBuilder: (context, index) => const Divider(),
+                itemBuilder: (context, index) =>
+                    PropertyWidget(property: properties[index]),
+              ),
+            ),
+          ],
         ),
       ),
     );
